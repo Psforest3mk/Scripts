8877f55a7456679ad26cc91b60a752b76b93ed0f
Psforest3mk <159436496+Psforest3mk@users.noreply.github.com>
2024-02-14 21:37:24 +0200

    Teleport.lua

--- Teleport.lua
+++ Teleport.lua
@@ -6,11 +6,11 @@
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
-local TPbox = Instance.new("TextBox")
+local TextLabel = Instance.new("TextLabel")
 local UICorner_2 = Instance.new("UICorner")
 local TPB = Instance.new("TextButton")
 local UICorner_3 = Instance.new("UICorner")
-local TextLabel = Instance.new("TextLabel")
+local TPbox = Instance.new("TextBox")
 local UICorner_4 = Instance.new("UICorner")
 
 --Properties:
@@ -28,39 +28,24 @@ UICorner.Parent = Frame
 
 Frame.Active = true
 Frame.Draggable = true
-Frame.Parent = gui
+Frame.Parent = ScreenGui
 
-TPbox.Name = "TPbox"
-TPbox.Parent = Frame
-TPbox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TPbox.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TPbox.BorderSizePixel = 0
-TPbox.Position = UDim2.new(0.247721687, 0, 0.38700375, 0)
-TPbox.Size = UDim2.new(0, 239, 0, 50)
-TPbox.Font = Enum.Font.SourceSansBold
-TPbox.Text = "Enter Target Player Name"
-TPbox.TextColor3 = Color3.fromRGB(0, 0, 0)
-TPbox.TextScaled = true
-TPbox.TextSize = 14.000
-TPbox.TextWrapped = true
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.0929648206, 0, 0.0484581515, 0)
+TextLabel.Size = UDim2.new(0, 323, 0, 50)
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Text = "Teleport script By Ps Forest"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextStrokeTransparency = 0.000
+TextLabel.TextWrapped = true
 
-UICorner_2.Parent = TPbox
-
-TPB.MouseButton1Down:Connect(function()
-	local playerName = TPbox.Text
-	local players = game:GetService("Players"):GetPlayers()
-	for i, player in ipairs(players) do
-		if player.Name:sub(1, #playerName):lower() == playerName:lower() then
-			game.Players.LocalPlayer.Character:MoveTo(player.Character.HumanoidRootPart.Position)
-			game:GetService("StarterGui"):SetCore("SendNotification",{
-				Title = "تم نقلك عند الاعب";
-				Text = "";
-				Duration = 3.5;
-			})
-			break
-		end
-	end
-end)
+UICorner_2.Parent = TextLabel
 
 TPB.Name = "TPB"
 TPB.Parent = Frame
@@ -78,21 +63,40 @@ TPB.TextWrapped = true
 
 UICorner_3.Parent = TPB
 
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(0.0929648206, 0, 0.0484581515, 0)
-TextLabel.Size = UDim2.new(0, 323, 0, 50)
-TextLabel.Font = Enum.Font.SourceSansBold
-TextLabel.Text = "Teleport script By Ps Forest"
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.TextScaled = true
-TextLabel.TextSize = 14.000
-TextLabel.TextStrokeTransparency = 0.000
-TextLabel.TextWrapped = true
+TPbox.Name = "TPbox"
+TPbox.Parent = Frame
+TPbox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TPbox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TPbox.BorderSizePixel = 0
+TPbox.Position = UDim2.new(0.247721687, 0, 0.38700375, 0)
+TPbox.Size = UDim2.new(0, 239, 0, 50)
+TPbox.Font = Enum.Font.SourceSansBold
+TPbox.Text = "Enter Target Player Name"
+TPbox.TextColor3 = Color3.fromRGB(0, 0, 0)
+TPbox.TextScaled = true
+TPbox.TextSize = 14.000
+TPbox.TextWrapped = true
 
-UICorner_4.Parent = TextLabel
+UICorner_4.Parent = TPbox
 
-coroutine.wrap(EHAVU_fake_script)()
+-- Scripts:
+
+local function BPTMPBK_fake_script() -- TPB.LocalScript 
+	local script = Instance.new('LocalScript', TPB)
+
+	local playerName = TPbox.Text
+	local players = game:GetService("Players"):GetPlayers()
+	for i, player in ipairs(players) do
+		if player.Name:sub(1, #playerName):lower() == playerName:lower() then
+			game.Players.LocalPlayer.Character:MoveTo(player.Character.HumanoidRootPart.Position)
+			game:GetService("StarterGui"):SetCore("SendNotification",{
+				Title = "تم نقلك عند الاعب";
+				Text = "";
+				Duration = 3.5;
+			})
+			break
+		end
+	end
+	
+end
+coroutine.wrap(BPTMPBK_fake_script)()

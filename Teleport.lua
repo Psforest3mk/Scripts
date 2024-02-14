835a541cf06abf5f77836553b866a6ea7319fe43
Psforest3mk <159436496+Psforest3mk@users.noreply.github.com>
2024-02-14 21:41:59 +0200

    Teleport.lua

--- Teleport.lua
+++ Teleport.lua
@@ -96,24 +96,4 @@ TPbox.TextWrapped = true
 
 UICorner_4.Parent = TPbox
 
--- Scripts:
-
-local function BPTMPBK_fake_script() -- TPB.LocalScript 
-	local script = Instance.new('LocalScript', TPB)
-
-	local playerName = TPbox.Text
-	local players = game:GetService("Players"):GetPlayers()
-	for i, player in ipairs(players) do
-		if player.Name:sub(1, #playerName):lower() == playerName:lower() then
-			game.Players.LocalPlayer.Character:MoveTo(player.Character.HumanoidRootPart.Position)
-			game:GetService("StarterGui"):SetCore("SendNotification",{
-				Title = "تم نقلك عند الاعب";
-				Text = "";
-				Duration = 3.5;
-			})
-			break
-		end
-	end
-	
-end
 coroutine.wrap(BPTMPBK_fake_script)()
